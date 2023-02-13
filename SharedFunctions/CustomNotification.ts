9f21bfc9015f25001db45777bd6b8171afca1a65
Ateeb Khan <ateebkhan997@gmail.com>
2023-02-13 10:39:53 +0500

    custom_notification

--- SharedFunctions/CustomNotification.ts
+++ SharedFunctions/CustomNotification.ts
@@ -1,6 +1,5 @@
 import { showNotification } from "@mantine/notifications";
 import { DefaultMantineColor } from "@mantine/styles";
-//
 type TitleProp = "Success" | "Failed";
 type NotificationProp = {
   title: TitleProp;
@@ -9,7 +8,6 @@ type NotificationProp = {
 export default function customNotification(prop: NotificationProp) {
   var isSuccess: boolean = prop.title == "Success";
   var color: DefaultMantineColor = isSuccess ? "green" : "red";
-
   showNotification({
     title: prop.title,
     message: prop.message,
@@ -23,10 +21,8 @@ export default function customNotification(prop: NotificationProp) {
           ? theme.colors.green[6]
           : theme.colors.red[6],
         borderColor: isSuccess ? theme.colors.green[6] : theme.colors.red[6],
-
         "&::before": { backgroundColor: theme.white },
       },
-
       title: { color: theme.white },
       description: { color: theme.white },
       closeButton: {
